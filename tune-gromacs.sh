99451bbadc16162a952e262e64fcd8804dba2ec1
rinnocente <rinnocente@users.noreply.github.com>
2017-05-11 11:50:18 +0200

    Update tune-gromacs.sh

--- tune-gromacs.sh
+++ tune-gromacs.sh
@@ -28,7 +28,7 @@ done
 if [ ! -d build-"$compile_flag" ]
 then
      GR_CORES=`cat /proc/cpuinfo |grep 'cpu cores'|uniq|sed -e 's/.*://'`
-     mkdir -p build-"$compile-flag" ; \
+     mkdir -p build-"$compile_flag" ; \
      (cd build-"$compile-flag"; cmake .. \
 	 -DGMX_SIMD="$compile-flag" -DCMAKE_C_COMPILER=mpicc -DCMAKE_CXX_COMPILER=mpicxx  \
 	 -DGMX_THREAD_MPI:BOOL=OFF -DGMX_MPI:BOOL=ON ; make -j $((2*GR_CORES)); \
